45eb36e881d49bed3adbf1801af7e97d81256755
LeonardoNaal <leonardoisraelnaalhernandez@gmail.com>
2017-02-16 01:15:41 -0600

    Agregue codigo
    agregue codigo para que pueda relizar el area y comprobar si es un
    triangulo

--- ProyectTriangulo/app/src/main/java/com/example/admin/proyecttriangulo/MainActivity.java
+++ ProyectTriangulo/app/src/main/java/com/example/admin/proyecttriangulo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.admin.proyecttriangulo;
 
+import android.support.annotation.FloatRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ladoA,ladoB,ladoC,areaTriangulo,validacion;
+    public float ResArea;
+    public boolean ResValidacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +22,25 @@ public class MainActivity extends AppCompatActivity {
         //Se crea la referencia al botón Calcular que mostrará el resultado del cálculo del área
         Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
 
+        ladoA=(EditText)findViewById(R.id.txtLadoA);
+        ladoB=(EditText)findViewById(R.id.txtLadoB);
+        ladoC=(EditText)findViewById(R.id.txtLadoC);
+        validacion=(EditText)findViewById(R.id.txtVerificar);
+        areaTriangulo=(EditText)findViewById(R.id.txtResultado);
+
         //Se crea la escucha anónima para ejecutar una acción al presionar el botón Verificar
         btnVerificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Triangulo objTriangulo=new Triangulo();
+                objTriangulo.LadoA= Float.parseFloat(ladoA.getText().toString());
+                objTriangulo.LadoB= Float.parseFloat(ladoB.getText().toString());
+                objTriangulo.LadoC= Float.parseFloat(ladoC.getText().toString());
+                CalcularAreaTriangulo objCalculoArea=new CalcularAreaTriangulo();
+                ResArea= objCalculoArea.CalcularArea(objTriangulo);
+                areaTriangulo.setText(Float.toString(ResArea));
                 //En esta parte se hace llamado al método para verificar si es un triángulo
                 //La respuesta se muestra en un TextView llamado txtVerificar
-
             }
         });
 
@@ -32,6 +48,18 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Triangulo objTriangulo=new Triangulo();
+                objTriangulo.LadoA= Float.parseFloat(ladoA.getText().toString());
+                objTriangulo.LadoB= Float.parseFloat(ladoB.getText().toString());
+                objTriangulo.LadoC= Float.parseFloat(ladoC.getText().toString());
+                ValidarTriangulo objValidar=new ValidarTriangulo();
+                ResValidacion= objValidar.Validar(objTriangulo);
+                if (ResValidacion==true){
+                    validacion.setText("Es un triángulo");
+                }
+                else {
+                    validacion.setText("No es un triángulo");
+                }
                 //En esta parte se hace llamado al método para calcular el area de un triángulo
                 //El resultado se muestra en un TextView llamado txtResultado
             }
