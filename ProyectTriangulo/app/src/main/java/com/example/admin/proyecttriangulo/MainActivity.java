6241ba8fd65923cbdf683e7d17cb40b64960f30f
David May <davisaac8@gmail.com>
2017-02-15 22:48:22 -0600

    Diseño
    Realicé el diseño de la aplicación, creé 3 campos para introducir los
    lados A. B y C; además agregué 2 botones: uno para verificar si es un
    triángulo y el otro para calcular el área.
    También creé la referencia a estos botones en el MainActivity y les
    agregué la escucha anónima para que haga algo cuando le den click.
    **Cambié los colores y el nombre, otra vez.

--- ProyectTriangulo/app/src/main/java/com/example/admin/proyecttriangulo/MainActivity.java
+++ ProyectTriangulo/app/src/main/java/com/example/admin/proyecttriangulo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.proyecttriangulo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se crea la referencia al botón Verificar que mostrará si es un triángulo o no
+        Button btnVerificar = (Button) findViewById(R.id.btnVerificar);
+
+        //Se crea la referencia al botón Calcular que mostrará el resultado del cálculo del área
+        Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+
+        //Se crea la escucha anónima para ejecutar una acción al presionar el botón Verificar
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //En esta parte se hace llamado al método para verificar si es un triángulo
+                //La respuesta se muestra en un TextView llamado txtVerificar
+
+            }
+        });
+
+        //Se crea la escucha anónima para ejecutar una acción al presionar el botón Calcular
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //En esta parte se hace llamado al método para calcular el area de un triángulo
+                //El resultado se muestra en un TextView llamado txtResultado
+            }
+        });
     }
 }
